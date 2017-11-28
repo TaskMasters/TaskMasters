d5d2168b89769b40f9fe4e3c78009c7a25ec3d33
Kristoffer <kristoffer.andersen96@gmail.com>
2017-11-28 13:23:31 +0100

    updated node version and styling

--- public/boards.js
+++ public/boards.js
@@ -18,3 +18,45 @@ fetch('/API/boards', {
   .catch(function(err) {
     console.log(err);
   });
+
+var color = [
+  '#FF7200',
+  '#ABBC4E',
+  '#A7E80C',
+  '#00a4bc',
+  '#FFD21A',
+  '#2E747F',
+  '#FFA45C',
+  '#4FFFB5'
+];
+
+addBoard();
+
+function addBoard() {
+  for (i = 0; i < 12; i++) {
+    var x = Math.floor(Math.random() * 8);
+
+    var newBoard = document.createElement('div');
+    newBoard.id = 'board';
+    newBoard.style.backgroundColor = color[x];
+    console.log(x);
+
+    var title = document.createElement('h1');
+    title.id = 'boardTitle';
+    var text = document.createTextNode('Board ' + (i + 1));
+    title.appendChild(text);
+    newBoard.appendChild(title);
+
+    var description = document.createElement('p');
+    description.id = 'description';
+    var descText = document.createTextNode(
+      'This is a description for board nr: ' + (i + 1)
+    );
+    description.appendChild(descText);
+    newBoard.appendChild(description);
+
+    var boards = document.getElementById('boards');
+
+    boards.appendChild(newBoard);
+  }
+}
